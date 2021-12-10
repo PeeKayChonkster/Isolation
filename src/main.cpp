7838680fc3284a9c95411800f7ef3922343d36bd
chonk <artstudioexp@gmail.com>
2021-12-10 21:44:21 +0300

    Refactoring. I went for the simpler project structure for start.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
-#include "raylib.h"
-#include <iostream>
+#include <raylib.h>
+
 
 int main()
 {
-    InitWindow(600, 600, "Катя, зноу палучилося, еще сильнее!");
+    InitWindow(600u, 600u, "chonky engine is where it's at");
+    Model temp = LoadModel("res/models/Computer.gltf");
+    Camera3D camera;
+    camera.position = Vector3{0.0f, 10.0f, 10.0f};
+    camera.target = Vector3{0.0f, 0.0f, 0.0f};
+    camera.up = Vector3{0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
     while(!WindowShouldClose())
     { 
         BeginDrawing();
-            ClearBackground(SKYBLUE);
+            ClearBackground(GRAY);
+            BeginMode3D(camera);
+            DrawModelEx(temp, Vector3{0.0f, 0.0f, 0.0f}, Vector3{1.0f, 0.0f, 0.0f}, 90.0f, Vector3{1.0f, 1.0f, 1.0f}, WHITE);
+            //DrawCube(Vector3(), 2.0f, 2.0f, 2.0f, GREEN);
+            
+            DrawGrid(10, 1.0f);
+
+            EndMode3D();
         EndDrawing();
     }
+
+    UnloadModel(temp);
     CloseWindow();
     return 0;
 }
